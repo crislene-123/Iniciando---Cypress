de9541687c034d6177627d1865fc16b4358faab7
crislene-123 <crislenevm@hotmail.com>
2025-06-29 02:58:34 -0300

    base url added

--- cypress.config.js
+++ cypress.config.js
@@ -1,17 +1,12 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  component: {
-    devServer: {
-      framework: "next",
-      bundler: "webpack",
-    },
-  },
-
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
-    },
+  },
+
+
     baseUrl: 'https://opensource-demo.orangehrmlive.com/web/index.php',
   },
 });
