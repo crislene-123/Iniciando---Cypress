e4b3daf9cf24f0bdf70b28fd7c04e7fc68499d6d
crislene-123 <crislenevm@hotmail.com>
2025-07-02 16:16:14 -0300

    User Update scenario added

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -1,4 +1,4 @@
-import userData from '../fixtures users/user-data.json'
+import userData from'../fixtures_users/users-data.json'
 
 describe('Orange HRM Tests', () => {
 
@@ -8,24 +8,46 @@ describe('Orange HRM Tests', () => {
     loginButton: "[type='submit']",
     sectionTitleTopbar: ".oxd-topbar-header-breadcrumb-module",
     dashboardGrid: ".orangehrm-dashboard-grid",
-    wrongCredentialAlert: "[role='alert']"
+    wrongCredentialAlert: "[role='alert']",
+    MyInfoButton:'[href="/web/index.php/pim/viewMyDetails"]',
+    firstNameField: "[name ='firstName']",
+    lastNameField: "[name ='lastName']",
+   genericField: ".oxd-input--active",
+   dateField: "[placeholder ='yyyy-mm-dd']",
+   dateCloseButton: ".--close",
+   submitButton: "[type='submit']"
   }
 
-  it('Login - Success', () => {
-    cy.visit('/auth/login')
-    cy.get(selectorList.usernameField).type(userData.userSuccess.username)
-    cy.get(selectorList.passwordField).type(userData.userSuccess.password)
-    cy.get(selectorList.loginButton).click()
-    cy.location('pathname').should('equal', '/web/index.php/dashboard/index')  
-    cy.get(selectorList.dashboardGrid)
+  it.only('User Info Update - Success', () => {
+    cy.fixture('users/user-data.json').then((userData) => {
+      cy.visit('/auth/login')
+      cy.get(selectorList.usernameField).type(userData.userSuccess.username)
+      cy.get(selectorList.passwordField).type(userData.userSuccess.password)
+      cy.get(selectorList.loginButton).click()
+      cy.location('pathname').should('equal', '/web/index.php/dashboard/index')  
+      cy.get(selectorList.dashboardGrid)
+      cy.get(selectorList.MyInfoButton).click()
+      cy.get(selectorList.firstNameField).type('FirstNameTest')
+      cy.get(selectorList.lastNameField).type('lastNameTest')
+      cy.get(selectorList.genericField).eq(3).clear().type('nickNameTest')
+      cy.get(selectorList.genericField).eq(4).clear().type('Employee')
+      cy.get(selectorList.genericField).eq(5).clear().type('OtherIdTest')
+      cy.get(selectorList.genericField).eq(6).clear().type('DriversLicenseTest')
+      cy.get(selectorList.genericField).eq(7).type('2025-03-10')
+      cy.get(selectorList.dateCloseButton).click()
+      cy.get(selectorList.genericField).eq(7).should('have.value', '2025-03-10')
+      cy.get(selectorList.genericField).eq(8).clear().type('ssnNumberTest')
+      cy.get(selectorList.genericField).eq(9).clear().type('sinNumberTest')
   })
-
   it('Login - Fail', () => {
-    cy.visit('/auth/login')
-    cy.get(selectorList.usernameField).type(userData.userFail.username)
-    cy.get(selectorList.passwordField).type(userData.userFail.password)
-    cy.get(selectorList.loginButton).click()
-    cy.get(selectorList.wrongCredentialAlert)
+    cy.fixture('users/user-data.json').then((userData) => {
+      cy.visit('/auth/login')
+      cy.get(selectorList.usernameField).type(userData.userFail.username)
+      cy.get(selectorList.passwordField).type(userData.userFail.password)
+      cy.get(selectorList.loginButton).click()
+      cy.get(selectorList.wrongCredentialAlert)
+    })
+  })
   })
 
 })
